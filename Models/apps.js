d1f5d9d99f0873be6a939579b4117b47a1642d40
Jennifer Morales <jennifer.morales1201@gmail.com>
2020-04-25 16:53:33 -0400

    Added another queary to the model and added another route

--- Models/apps.js
+++ Models/apps.js
@@ -1,8 +1,6 @@
 const db = require("./conn.js");
 class Apps {
-  
   static async getAllApplications(user_id) {
-    console.log("calling allapplications method")
     try {
       const response = await db.any(
         `select * from applications where applications.user_id=${user_id};`
@@ -12,6 +10,18 @@ class Apps {
       return err.message;
     }
   }
+
+  static async getAllData(user_id) {
+    try {
+      const response = await db.any(
+        `SELECT * FROM users INNER JOIN applications ON users.id = applications.user_id INNER JOIN companies ON companies.id = applications.company_id WHERE users.id=${user_id};`
+      );
+      return response;
+    } catch (err) {
+      return err.message;
+    }
+  }
+
   static async getPublicApplications() {
     try {
       const response = await db.any(
@@ -25,4 +35,3 @@ class Apps {
 }
 
 module.exports = Apps;
-
