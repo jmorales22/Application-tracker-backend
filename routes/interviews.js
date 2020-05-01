fc1669b168721d20ad61cadbb6ac504cc1353e31
Ally Brannon <allison.brannon@gmail.com>
2020-05-01 10:35:02 -0400

    fixed var to const

--- routes/interviews.js
+++ routes/interviews.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const Interviews = require("../Models/interviews.js");
 
 router.get("/:application_id?", async function (req, res) {
