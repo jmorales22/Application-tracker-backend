db9f8db7e0a5f2cfa170a29b3686d5a29fb4370c
Ally Brannon <allison.brannon@gmail.com>
2020-05-01 10:27:33 -0400

    updated interview routes with require

--- routes/interviews.js
+++ routes/interviews.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var router = express.Router();
+const Interviews = require("../Models/interviews.js");
 
 router.get("/:application_id?", async function (req, res, next) {
   const { application_id } = req.params;
-  
+
   let data = await Interviews.getInterviews(application_id);
   res.json(data);
 });
